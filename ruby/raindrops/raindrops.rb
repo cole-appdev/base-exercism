4042c294f4da61cc1587f29ee7a8de1ba7f2a869
johnson-cole <colejohnson@chicagobooth.edu>
2021-04-09 00:33:47 +0000

    Final commit without skips, failures, or errors

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -6,3 +6,26 @@ To get started with TDD, see the `README.md` file in your
 `ruby/raindrops` directory.
 =end
 
+class Raindrops
+
+    def self.convert(val)
+        ans = ""
+
+        if val % 3 == 0 
+            ans << "Pling"
+        end
+        if val % 5 == 0
+            ans << "Plang"
+        end
+        if val % 7 == 0
+            ans << "Plong"
+        end
+
+        if ans == ""
+            return val.to_s
+        else
+            return ans
+        end
+
+    end
+end
